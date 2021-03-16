0ad98dc07d9127126c7ba3122a2136a059d8c80e
John Frankolin <johfr13@liu.se>
2021-03-16 16:53:05 +0100

    test

--- starter/src/utility/auth/MsalConfig.js
+++ starter/src/utility/auth/MsalConfig.js
@@ -17,7 +17,7 @@ export const msalConfig = {
 export const loginRequest = {
     scopes: ["openid", "profile", "User.Read","https://liuonline.onmicrosoft.com/ida-spacy-progress-api-test-ar/user_impersonation"],
     forceRefresh: false // Set this to "true" to skip a cached token and go to the server to get a new token
-};
+}; 
  
 // Add here scopes for id token to be used at MS Identity Platform endpoints.
 export const apiRequest = {
